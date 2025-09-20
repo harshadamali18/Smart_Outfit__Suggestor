ebb430cbb572309029243fb931ad5caaa0228393
harshadamali18 <harshadamali1812@gmail.com>
2025-09-21 03:54:21 +0530

    Initial commit

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def weather():
     return render_template("index.html", weather=weather_desc, outfit=outfit)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
